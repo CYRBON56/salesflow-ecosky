b1394023be6b9b2e21e8a5c7679c3492ff8da9db
CYRBON56 <c.bon@ecosky.fr>
2026-07-18 14:00:10 +0200

    Add MobileSummary component for mobile view

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import MobileSummary from "./MobileSummary.jsx";
+
+const isMobileView = window.location.pathname.replace(/\/+$/, "") === "/mobile";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    {isMobileView ? <MobileSummary /> : <App />}
   </React.StrictMode>
 );
